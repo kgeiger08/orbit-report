55a00ba4892ba025c4521d3b5ab3b8ef4d67630f
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-05 00:32:56 -0600

    added code for step 4 still working on ngFor part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'orbit-report';
