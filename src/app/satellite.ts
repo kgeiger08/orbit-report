e1d11218b8ea7f6bc612246be736f9be8bb7f9fc
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-05 21:24:13 -0600

    Step 6 work in progress

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -11,6 +11,14 @@ export class Satellite {
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
-    }
+    };
 
-}
+    // shouldShowWarning(): boolean {
+    //     if(this.type.toLowerCase === "space debris") {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     };
+    // };
+
+};
